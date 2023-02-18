5ff5efb2b71b697ad3978134feb5e08703d642e6
abrubel <abdur.rahaman.ewu082@gmail.com>
2023-02-18 20:01:57 +0600

    Style: Display icon on hover

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 function Image({ className, img }) {
   const [hover, setHover] = React.useState(false);
-  console.log(hover);
+  const heartIcon = hover && <i className="ri-heart-line favorite"></i>;
+  const cartIcon = hover && <i className="ri-add-circle-line cart"></i>;
   return (
     <div
-      className={`${className} image-container `}
+      className={`${className} image-container ${hover} (hover ? 'show' : 'hidden')`}
       onMouseOver={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
       <img className="image-grid" src={img.url} alt="" />
+      {heartIcon}
+      {cartIcon}
     </div>
   );
 }
