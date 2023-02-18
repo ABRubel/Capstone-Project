40371340db15805a252cdb3a32ec47d8b68d6ce9
abrubel <abdur.rahaman.ewu082@gmail.com>
2023-02-18 19:53:47 +0600

    feat: Add track hover state

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 function Image({ className, img }) {
+  const [hover, setHover] = React.useState(false);
+  console.log(hover);
   return (
-    <div className={`${className} image-container `}>
+    <div
+      className={`${className} image-container `}
+      onMouseOver={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <img className="image-grid" src={img.url} alt="" />
     </div>
   );
