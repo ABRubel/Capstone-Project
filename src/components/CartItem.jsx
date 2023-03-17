09ed8abb6677654d8d83db947e44b252eac21c8e
abrubel <abdur.rahaman.ewu082@gmail.com>
2023-03-18 02:56:46 +0600

    feat: Add remove item from cart functionality

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { Context } from "../Context";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 function CartItem({ item }) {
+  const { removeFromCart } = React.useContext(Context);
+  const MySwal = withReactContent(Swal);
+  function handleDelete(id) {
+    MySwal.fire({
+      title: "Are you sure?",
+      text: "You will not be able to recover this item!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, keep it",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        removeFromCart(id);
+        MySwal.fire("Deleted!", "Your item has been deleted.", "success");
+      } else if (result.dismiss === MySwal.DismissReason.cancel) {
+        MySwal.fire("Cancelled", "Your item is safe :)", "error");
+      }
+    });
+  }
+
   return (
     <div className="cart-item">
-      <i className="ri-delete-bin-line"></i>
+      <i
+        className="ri-delete-bin-line"
+        onClick={() => handleDelete(item.id)}
+      ></i>
       <img src={item.url} width="130px" />
       <p>$5.99</p>
     </div>
